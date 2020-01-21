1b8a0244cfb01bb2412a1887270b5d6f20a7a354
NeroShredder <41788041+NeroShredder@users.noreply.github.com>
2020-01-21 11:50:20 +0200

    test

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function About() {
+    return(
+        <div>
+            <h1>This is a sad attempt to use OMDB api </h1>
+        </div>
+    );
+}
+
+export default About;
