740fe944c9955753c9f9d942792b03067480b88b
NeroShredder <41788041+NeroShredder@users.noreply.github.com>
2020-01-27 11:35:06 +0200

    Added buttons and textlinks with interactions. Installed eva icons.
    Note to self (index.js): import "eva-icons/style/eva-icons.css";

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import './footer.scss';
-
-
-
+import React from "react";
+import "./footer.scss";
 
 function Footer() {
-    return(
-        <>
-            <footer>
-                <h1>This is a sad attempt to use Footer</h1>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer>
+        <h1>This is a sad attempt to use Footer</h1>
+        <button type="button" className="btn-hero-disabled">
+          <i class="eva eva-shopping-cart"></i>
+          Primary Button
+        </button>
+        <button type="button" className="btn-hero">
+          <i class="eva eva-shopping-cart-outline"></i>
+          Primary Button
+        </button>
+        <button type="button" className="btn-grey">
+          Secondary Button
+        </button>
+        <button type="button" className="btn-grey-disabled">
+          Secondary Button
+        </button>
+        <button type="button" className="btn-hero-round">
+          <i class="eva  eva-2x eva-edit-outline"></i>
+        </button>
+        <button type="button" className="btn-hero-round-disabled">
+          <i class="eva  eva-2x eva-edit"></i>
+        </button>
+        <button type="button" className="btn-grey-round">
+          <i class="eva  eva-2x eva-edit"></i>
+        </button>
+        <button type="button" className="btn-grey-round-disabled">
+          <i class="eva  eva-2x eva-edit"></i>
+        </button>
+        <h1>Links</h1>
+        <a href="#" className="textlink-1">
+          Link 1
+        </a>
+        <a href="#" className="textlink-disabled">
+          Link disabled
+        </a>
+        <a href="#" className="textlink-2">
+          Link 2
+        </a>
+        <a href="#" className="textlink-3">
+          Link 3
+        </a>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
