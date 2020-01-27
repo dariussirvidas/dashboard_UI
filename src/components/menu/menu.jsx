2de73a3d45dd25e8eff31eb87a2b7633ac87c191
Donatas Ivaskevicius <donatas.ivaskevicius@gmail.com>
2020-01-27 17:35:08 +0200

    Fixed menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import FestoLogo from '../.././Content/FestoLogo.png'
 import menu from './menu.scss';
+import {Link} from "react-router-dom";
 
 
 
@@ -11,23 +12,17 @@ class Menu extends Component {
                 <img className="Logo" src={FestoLogo} alt="Festo"/>
                 <h1 className="headerText cl-h1">Monitoring Dashboard</h1>
                 <h1 className="headerUser cl-h1"> User: user567</h1>
-
-
                 <input className="menu-btn" type="checkbox" id="menu-btn"/>
                 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
                 <ul className="menu">
-                    <li><a href="#HomePage">Home Page</a></li>
-                    <li><a href="#MaintainingList">Maintaining list</a></li>
-                    <li><a href="#Comment">Comment</a></li>
-                    <li><a href="#Users">Users</a></li>
+                    <li><Link to="/">Home Page</Link></li>
+                    <li><Link to="/domains">Maintaining list</Link></li>
+                    <li><Link to="/topics">Comment</Link></li>
+                    <li><Link to="/">Users</Link></li>
                 </ul>
             </div>
-
         );
-
     }
-
-
 }
 
 export default Menu;
