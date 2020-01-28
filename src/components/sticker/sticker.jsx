62ac695d42c7281372d4715aa272501261ecf86c
NeroShredder <41788041+NeroShredder@users.noreply.github.com>
2020-01-28 12:55:11 +0200

    added media queries to sticker.scss

--- src/components/sticker/sticker.jsx
+++ src/components/sticker/sticker.jsx
@@ -3,7 +3,7 @@ import "./sticker.scss";
 
 function Sticker() {
   return (
-    <div>
+    <div className="sticker-container">
       <div className="tile-success">
         <h3 className="cl-h3">Service name</h3>
         <p className="cl-copy-14">Response time:9000ms </p>
@@ -15,6 +15,42 @@ function Sticker() {
         <p className="cl-copy-14">Response time: </p>
         <p className="cl-copy-14">Last Failure: </p>
         <p className="cl-copy-14">Next Check in: </p>
+      </div>
+      <div className="tile-success">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time:4000ms </p>
+        <p className="cl-copy-14">Last Failure: Yesturday </p>
+        <p className="cl-copy-14">Next Check in: Today </p>
+      </div>
+      <div className="tile-success">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time:9000ms </p>
+        <p className="cl-copy-14">Last Failure: Yesturday </p>
+        <p className="cl-copy-14">Next Check in: Today </p>
+      </div>
+      <div className="tile-fail">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time: </p>
+        <p className="cl-copy-14">Last Failure: </p>
+        <p className="cl-copy-14">Next Check in: </p>
+      </div>
+      <div className="tile-success">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time:4000ms </p>
+        <p className="cl-copy-14">Last Failure: Yesturday </p>
+        <p className="cl-copy-14">Next Check in: Today </p>
+      </div>
+      <div className="tile-fail">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time: </p>
+        <p className="cl-copy-14">Last Failure: </p>
+        <p className="cl-copy-14">Next Check in: </p>
+      </div>
+      <div className="tile-success">
+        <h3 className="cl-h3">Service name</h3>
+        <p className="cl-copy-14">Response time:4000ms </p>
+        <p className="cl-copy-14">Last Failure: Yesturday </p>
+        <p className="cl-copy-14">Next Check in: Today </p>
       </div>
     </div>
   );
