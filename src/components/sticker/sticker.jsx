d7b873e8fa90bd3ca7050ec199e92048079698b0
dzemaitis <dellas@localhost.localdomain>
2020-02-13 10:31:20 +0200

    ping changed to portal/service

--- src/components/sticker/sticker.jsx
+++ src/components/sticker/sticker.jsx
@@ -6,20 +6,26 @@ function Sticker(props) {
       <div className={
           "tile-unclear " +
           (props.domainPing.status === "TimedOut" && "tile-fail ") + " " +
-          (props.domainPing.status === "Success" && "tile-success ") + " " +
+          (props.domainPing.status === 200 && "tile-success ") + " " +
           ((props.domainPing.message !== undefined && "tile-fail "))
       }
       >
           <h3 className="cl-h3">Service name: {props.item.service_Name}</h3>
           <p className="cl-copy-14">
+              URL:
+              {props.domainPing.domainUrl}
               Response time:
               {
+                  
                   props.domainPing &&
                   <>
-                      {props.domainPing.latencyMS}
+                      {props.domainPing.requestTime}
                   </>
 
               }
+              Status:
+              {props.domainPing.status}
+
           </p>
           {/*<p className="cl-copy-14">Response code: {domainPingResponseCode}</p>*/}
           <p className="cl-copy-14">Last Failure: {props.item.last_Fail.slice(0, 10)} {props.item.last_Fail.slice(11, 16)}</p>
